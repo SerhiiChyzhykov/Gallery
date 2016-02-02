73238cfa49f9d15ec58e1c2634cf49a06be42e45
GriOMG <defacto@live.ru>
2016-02-02 21:05:37 +0200

    Update Symfony 3.0

---
+++ src/AppBundle/Entity/categories.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace AppBundle\Entity;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * categories
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class categories
+{
+      /**
+     * @ORM\OneToMany(targetEntity="photo", mappedBy="categories")
+     */
+    protected $photo;
+
+    public function __construct()
+    {
+        $this->photo = new ArrayCollection();
+    }
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return categories
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Add photo
+     *
+     * @param \AppBundle\Entity\photo $photo
+     *
+     * @return categories
+     */
+    public function addPhoto(\AppBundle\Entity\photo $photo)
+    {
+        $this->photo[] = $photo;
+
+        return $this;
+    }
+
+    /**
+     * Remove photo
+     *
+     * @param \AppBundle\Entity\photo $photo
+     */
+    public function removePhoto(\AppBundle\Entity\photo $photo)
+    {
+        $this->photo->removeElement($photo);
+    }
+
+    /**
+     * Get photo
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getPhoto()
+    {
+        return $this->photo;
+    }
+}
---
+++ src/AppBundle/Entity/photo.php
@@ -0,0 +1,215 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * photo
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class photo
+{
+
+         /**
+     * @ORM\ManyToOne(targetEntity="categories", inversedBy="photo")
+     * @ORM\JoinColumn(name="categories_id", referencedColumnName="id")
+     */
+    protected $categories;
+        /**
+     * @ORM\ManyToOne(targetEntity="user", inversedBy="photo")
+     * @ORM\JoinColumn(name="username_id", referencedColumnName="id")
+     */
+    protected $username;
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="image", type="string", length=255)
+     */
+    private $image;
+
+    
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return photo
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return photo
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set image
+     *
+     * @param string $image
+     *
+     * @return photo
+     */
+    public function setImage($image)
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    /**
+     * Get image
+     *
+     * @return string
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    
+
+    /**
+     * Set categories
+     *
+     * @param \AppBundle\Entity\categories $categories
+     *
+     * @return photo
+     */
+    public function setCategories(\AppBundle\Entity\categories $categories = null)
+    {
+        $this->categories = $categories;
+
+        return $this;
+    }
+
+    /**
+     * Get categories
+     *
+     * @return \AppBundle\Entity\categories
+     */
+    public function getCategories()
+    {
+        return $this->categories;
+    }
+
+    /**
+     * Set user
+     *
+     * @param \AppBundle\Entity\user $user
+     *
+     * @return photo
+     */
+    public function setUser(\AppBundle\Entity\user $user = null)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get user
+     *
+     * @return \AppBundle\Entity\user
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+   
+    /**
+     * Set username
+     *
+     * @param \AppBundle\Entity\user $username
+     *
+     * @return photo
+     */
+    public function setUsername(\AppBundle\Entity\user $username = null)
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * Get username
+     *
+     * @return \AppBundle\Entity\user
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+}
