a07e81146dc23e47a0255a17f01926c153520487
GriOMG <defacto@live.ru>
2016-03-06 23:47:19 +0200

    Add paginator

--- src/AppBundle/Controller/HomeController.php
+++ src/AppBundle/Controller/HomeController.php
@@ -14,53 +14,35 @@ class HomeController extends Controller
     /**
      *@Route("/",name="home")
      */
-    public function homeAction(Request $request, $page = 1)
+    public function homeAction(Request $request)
     {
 
- $message = [];
+      $message = [];
       $data = $request->get('search');
       $repository = $this->getDoctrine()
       ->getRepository('AppBundle:photo');
       $query = $repository->createQueryBuilder('p')
       ->where('p.title LIKE :title')
       ->setParameter('title','%'.$data.'%')
-      ->setMaxResults('8')
       ->getQuery();
 
       $ser = $query->getResult();
+      $paginator  = $this->get('knp_paginator');
+      $pagination = $paginator->paginate(
+        $ser,$request->query->getInt('page', 1),8);
 
       if (!$ser) {
        $message['danger'] = '
-Search No results: ' .$data;
-      }elseif(!empty($data)){ $message['success'] = '
-Search: ' .$data;}
+       Search No results: ' .$data;
+     }elseif(!empty($data)){ $message['success'] = '
+     Search: ' .$data;}
 
-$repo = $this   ->getDoctrine()
-                ->getManager()
-                ->getRepository('AppBundle:photo');
-
-$qb = $repo->createQueryBuilder('a')
-        ->select('COUNT(a)')
-      ->where('a.title LIKE :title')
-      ->setParameter('title','%'.$data.'%');
-
-$count = $qb->getQuery()->getSingleScalarResult();
-
-$limit = 8;
-    $maxPages = ceil($count / $limit);
-    $thisPage = $page;
-
-
-return $this->render('design/index.html.twig', array(
-  'message' =>$message,
-  'maxPages' => $maxPages,
-  'thisPage' =>  $thisPage,
-  'photo' => $ser,
-  'title' => 'Home',
-  'url' => 'home',
-  'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
-  ));
-}
-
-
-}
+     return $this->render('design/index.html.twig', array(
+       'message' =>$message,
+       'title' => 'Home',
+       'url' => 'home',
+       'pagination' => $pagination,
+       'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+       ));
+   }
+ }
