ddd16ffa54d5f9659a0820dd325cc70a42f517d3
omggris <defacto@live.ru>
2016-08-15 18:09:32 +0300

    Update : Admin area v 0.2

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -11,8 +11,15 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 class AdminController extends Controller
 {
+
+  public function user(){
+    $user =  $this->get('security.token_storage')->getToken()->getUser();
+
+    return $user;
+  }
+
     /**
-     *@Route("/profile",name="profile")
+     *@Route("/admin",name="admin")
      */
     public function homeAction(Request $request)
     {
@@ -51,87 +58,262 @@ class AdminController extends Controller
 
     $photos = $qb->getQuery()->getSingleScalarResult();
     endif;
-
-    return $this->render('design/admin/index.html.twig', array(
-     'message' =>$message,
-     'title' => 'Home',
-     'url' => 'home',
-     'photos' => $photos ,
-     'pagination' => $pagination,
-     'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
-     ));
+    if($user != 'anon'):
+      return $this->render('design/admin/index.html.twig', array(
+       'message' =>$message,
+       'title' => 'Dashboard',
+       'url' => 'home',
+       'photos' => $photos ,
+       'pagination' => $pagination,
+       'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+       ));
+    else:
+      return $this->redirectToRoute('home');
+    endif;
   }
 
      /**
-     *@Route("/my_profile",name="my_profile")
+     *@Route("/admin/profile",name="admin_profile")
      */
      public function profileAction(Request $request)
      {
+      $user =  $this->get('security.token_storage')->getToken()->getUser();
 
-      $message = [];
-      $data = $request->get('search');
+      $repository = $this->getDoctrine()
+      ->getRepository('AppBundle:user');
+
+      $query = $repository->createQueryBuilder('p')
+      ->where('p.id = :id')
+      ->setMaxResults(1)
+      ->setParameter('id',$user )
+      ->getQuery();
+
+      $user_profile = $query->getResult();
+
       $repository = $this->getDoctrine()
       ->getRepository('AppBundle:photo');
       $query = $repository->createQueryBuilder('p')
-      ->where('p.title LIKE :title')
-      ->setParameter('title','%'.$data.'%')
+      ->where('p.username = :id')
+      ->setParameter('id',$user )
+      ->getQuery();
+      $photo = $query->getResult();
+
+      $qb = $repository->createQueryBuilder('a');
+      $qb->select('COUNT(a)');
+      $qb->where('a.username = :usernameId');
+      $qb->setParameter('usernameId', $user );
+
+      $photos = $qb->getQuery()->getSingleScalarResult();
+
+
+      if($user != 'anon'):
+        return $this->render('design/admin/user.html.twig', array(
+         'user'     => $user_profile,
+         'photo'    => $photo,
+         'photos'   => $photos,
+         'title' => $user,
+         'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+         ));
+      else:
+        return $this->redirectToRoute('home');
+      endif;
+    }
+
+    /**
+     *@Route("/admin/profile/photo",name="admin_my_photo")
+     */
+    public function  my_photosAction(Request $request)
+    {
+      $user =  $this->get('security.token_storage')->getToken()->getUser();
+      $repository = $this->getDoctrine()
+      ->getRepository('AppBundle:photo');
+      $query = $repository->createQueryBuilder('p')
+      ->where('p.username = :id')
+      ->setParameter('id',$user)
+      ->getQuery();
+      $photo = $query->getResult();
+
+      if($user != 'anon'):
+        return $this->render('design/admin/my_photos.html.twig', array(
+         'photo' => $photo,
+         'title' => '$user',
+         'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+         ));
+      else:
+        return $this->redirectToRoute('home');
+      endif;
+    }
+
+    /**
+     *@Route("/admin/users",name="admin_users")
+     */
+    public function  usersAction(Request $request)
+    {
+      $user =  $this->get('security.token_storage')->getToken()->getUser();
+      $repository = $this->getDoctrine()
+      ->getRepository('AppBundle:user');
+      $query = $repository->createQueryBuilder('p')->getQuery();
+      $users = $query->getResult();
+
+      if($user != 'anon'):
+        return $this->render('design/admin/users.html.twig', array(
+         'users' => $users,
+         'title' => 'Users',
+         'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+         ));
+      else:
+        return $this->redirectToRoute('home');
+      endif;
+    }
+
+    /**
+     *@Route("/admin/user/{id}",name="admin_user")
+     */
+    public function  userAction(Request $request,$id)
+    {
+      $user =  $this->get('security.token_storage')->getToken()->getUser();
+
+      $repository = $this->getDoctrine()
+      ->getRepository('AppBundle:user');
+
+      $query = $repository->createQueryBuilder('p')
+      ->where('p.id = :id')
+      ->setMaxResults(1)
+      ->setParameter('id',$id)
       ->getQuery();
 
-      $ser = $query->getResult();
-      $paginator  = $this->get('knp_paginator');
-      $pagination = $paginator->paginate(
-        $ser,$request->query->getInt('page', 1),8);
+      $user_profile = $query->getResult();
 
-      if (!$ser) {
-       $message['danger'] = '
-       Search No results: ' .$data;
-     }elseif(!empty($data)){ $message['success'] = '
-     Search: ' .$data;}
+      $repository = $this->getDoctrine()
+      ->getRepository('AppBundle:photo');
+      $query = $repository->createQueryBuilder('p')
+      ->where('p.username = :id')
+      ->setParameter('id',$id)
+      ->getQuery();
+      $photo = $query->getResult();
 
-     $user =  $this->get('security.token_storage')->getToken()->getUser();
+      $qb = $repository->createQueryBuilder('a');
+      $qb->select('COUNT(a)');
+      $qb->where('a.username = :usernameId');
+      $qb->setParameter('usernameId', $id);
 
-     if($user):
+      $photos = $qb->getQuery()->getSingleScalarResult();
 
-      $repo = $this->getDoctrine()->getManager()
-    ->getRepository('AppBundle:photo');
 
-    $qb = $repo->createQueryBuilder('a');
-    $qb->select('COUNT(a)');
-    $qb->where('a.username = :usernameId');
-    $qb->setParameter('usernameId', $user);
+      if($user != 'anon'):
+        return $this->render('design/admin/user.html.twig', array(
+         'user'     => $user_profile,
+         'photo'    => $photo,
+         'photos'   => $photos,
+         'title' => $user,
+         'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+         ));
+      else:
+        return $this->redirectToRoute('home');
+      endif;
+    }
 
-    $photos = $qb->getQuery()->getSingleScalarResult();
-    endif;
-
-    return $this->render('design/admin/profile.html.twig', array(
-     'message' =>$message,
-     'title' => 'Home',
-     'url' => 'home',
-     'photos' => $photos ,
-     'pagination' => $pagination,
-     'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
-     ));
-  }
-
-   /**
-     *@Route("/my_photos",name="my_photos")
+    /**
+     *@Route("/admin/photos",name="admin_photos")
      */
-   public function  my_photosAction(Request $request)
-   {
+    public function  photosAction(Request $request)
+    {
+      $user =  $this->get('security.token_storage')->getToken()->getUser();
+      $repository = $this->getDoctrine()
+      ->getRepository('AppBundle:photo');
+      $query = $repository->createQueryBuilder('p')
+      ->where('p.username = :id')
+      ->setParameter('id',$user)
+      ->getQuery();
+      $photo = $query->getResult();
 
-    $user =  $this->get('security.token_storage')->getToken()->getUser();
-    $repository = $this->getDoctrine()
-    ->getRepository('AppBundle:photo');
-    $query = $repository->createQueryBuilder('p')
-        ->where('p.username = :id')
-        ->setParameter('id',$user)
-    ->getQuery();
-    $photo = $query->getResult();
+      if($delete = $request->get('delete')):
+        $em = $this->getDoctrine()->getManager();
+      $delete = $em->getRepository('AppBundle:photo')->findOneById($delete);
+      $em->remove($delete);
+      $em->flush();
+      return $this->redirectToRoute('admin_photos');
+      endif;
+      
+      if($user != 'anon'):
+        return $this->render('design/admin/photos.html.twig', array(
+         'photo' => $photo,
+         'user' => $user,
+         'title' => 'Photos',
+         'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+         ));
+      else:
+        return $this->redirectToRoute('home');
+      endif;
+    }
 
-    return $this->render('design/admin/my_photos.html.twig', array(
-     'photo' => $photo,
-     'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
-     ));
+    /**
+     *@Route("/admin/new_user",name="admin_new_user")
+     */
+    public function new_userAction(Request $request)
+    {
+
+      $user =  $this->get('security.token_storage')->getToken()->getUser();
+
+      $username   = $request->get('username');
+      $last_name  = $request->get('last_name');
+      $first_name = $request->get('first_name');
+      $twitter = $request->get('twitter');
+      $password = $request->get('password');
+      $git = $request->get('git');
+      $google = $request->get('google');
+      $avatar = $request->get('avatar');
+
+      function is_valid_type($file)
+      {
+
+        $valid_types = array("image/jpg","image/jpeg", "image/bmp", "image/gif", "image/png");
+
+        if (in_array($file['type'], $valid_types))
+          return 1;
+        return 0;
+      }
+      if (isset($_FILES['image'])) {
+        if (!empty($_FILES['image'])) {
+          if (is_valid_type($_FILES['image']))
+          {
+            if (!file_exists($_FILES['image']['name']))
+            {
+              $extension = strtolower(substr(strrchr($_FILES['image']['name'], '.'), 1));
+              $filename = DFileHelper::getRandomFileName($extension);
+              $target =  'img/' . $filename . '.' . $extension;
+              if (move_uploaded_file($_FILES['image']['tmp_name'],$target)){
+                $user = new User();
+                $em = $this->getDoctrine()->getManager();
+                $user->setUsername($username);
+                $user->setlast_name($last_name);
+                $user->settwitter($twitter);
+                $user->setgoogle($google);
+                $user->setgit($git);
+                $user->setpassword($password);
+                $user->setfirst_name($first_name);
+                $user->setavatar('img/'.$filename. '.' . $extension);
+                $em->persist($user);
+                $em->flush();
+                $message['success'] = "Photo added";
+              }else{$message['danger'] = "You can not download the file. Check permissions to the directory ( read / write)";}
+            }else{$message['danger'] = "File with this name already exists";}
+          }else{$message['danger'] = "You can upload files : JPEG, GIF, BMP, PNG";}
+        }
+      }
+      
+      if($user != 'anon'):
+        return $this->render('design/admin/new_user.html.twig', array(
+         'user' => $user,
+         'title' => 'Photos',
+        'message' => $message,
+         'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+         ));
+      else:
+        return $this->redirectToRoute('home');
+      endif;
+    }
+
   }
 
-}
+
--- src/AppBundle/Entity/user.php
+++ src/AppBundle/Entity/user.php
@@ -13,10 +13,10 @@ class user implements UserInterface, \Serializable
      /**
      * @ORM\OneToMany(targetEntity="post", mappedBy="user")
      */
-    protected $post;
+     protected $post;
 
-    public function __construct()
-    {
+     public function __construct()
+     {
         $this->post = new ArrayCollection();
     }
     /**
@@ -35,6 +35,42 @@ class user implements UserInterface, \Serializable
      * @ORM\Column(type="string", length=64)
      */
     private $password;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $avatar;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $last_name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    
+    private $first_name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $git;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $twitter;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $google;
+
+    /**
+     * @ORM\Column(type="integer", length=10)
+     */
+    private $rank;
 
     public function getUsername()
     {
@@ -71,7 +107,7 @@ class user implements UserInterface, \Serializable
             $this->password,
             // see section on salt below
             // $this->salt,
-        ));
+            ));
     }
 
     /** @see \Serializable::unserialize() */
@@ -83,7 +119,7 @@ class user implements UserInterface, \Serializable
             $this->password,
             // see section on salt below
             // $this->salt
-        ) = unserialize($serialized);
+            ) = unserialize($serialized);
     }
 
     /**
@@ -111,6 +147,173 @@ class user implements UserInterface, \Serializable
     }
 
     /**
+     * Get avatar
+     *
+     * @return string
+     */
+    public function getavatar()
+    {
+        return $this->avatar;
+    }
+
+    /**
+     * Set avatar
+     *
+     * @param string $avatar
+     *
+     * @return user
+     */
+    public function setavatar($avatar)
+    {
+        $this->avatar = $avatar;
+
+        return $this;
+    }
+
+    /**
+     * Get rank
+     *
+     * @return integer
+     */
+    public function getrank()
+    {
+        return $this->rank;
+    }
+
+    /**
+     * Set rank
+     *
+     * @param integer $rank
+     *
+     */
+    public function setrank($rank)
+    {
+        $this->rank = $rank;
+
+        return $this;
+    }
+
+
+    /**
+     * Get last_name
+     *
+     * @return string
+     */
+    public function getlast_name()
+    {
+        return $this->last_name;
+    }
+
+    /**
+     * Set last_name
+     *
+     * @param string $last_name
+     *
+     * @return user
+     */
+    public function setlast_name($last_name)
+    {
+        $this->last_name = $last_name;
+
+        return $this;
+    }
+
+        /**
+     * Get first_name
+     *
+     * @return string
+     */
+        public function getfirst_name()
+        {
+            return $this->first_name;
+        }
+
+    /**
+     * Set first_name
+     *
+     * @param string $first_name
+     *
+     * @return user
+     */
+    public function setfirst_name($first_name)
+    {
+        $this->first_name = $first_name;
+
+        return $this;
+    }
+
+    /**
+     * Get avatar
+     *
+     * @return string
+     */
+    public function getgit()
+    {
+        return $this->git;
+    }
+
+    /**
+     * Set git
+     *
+     * @param string $git
+     *
+     * @return user
+     */
+    public function setgit($git)
+    {
+        $this->git = $git;
+
+        return $this;
+    }
+
+        /**
+     * Get google
+     *
+     * @return string
+     */
+        public function getgoogle()
+        {
+            return $this->google;
+        }
+
+    /**
+     * Set google
+     *
+     * @param string $google
+     *
+     * @return user
+     */
+    public function setgoogle($google)
+    {
+        $this->google = $google;
+
+        return $this;
+    }
+    /**
+     * Get twitter
+     *
+     * @return string
+     */
+    public function gettwitter()
+    {
+        return $this->twitter;
+    }
+
+    /**
+     * Set twitter
+     *
+     * @param string $twitter
+     *
+     * @return user
+     */
+    public function settwitter($twitter)
+    {
+        $this->twitter = $twitter;
+
+        return $this;
+    }
+
+    /**
      * Set password
      *
      * @param string $password
@@ -124,7 +327,7 @@ class user implements UserInterface, \Serializable
         return $this;
     }
 
-  
+
 
     /**
      * Add post
