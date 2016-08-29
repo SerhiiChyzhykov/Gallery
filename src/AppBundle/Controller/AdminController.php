641a2d409471e8f09d1b8635d3acabafdb8be3eb
omggris <defacto@live.ru>
2016-08-29 17:11:57 +0300

    Update:Profile

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -108,7 +108,6 @@ class AdminController extends Controller
        $last_name  = $request->get('last_name');
        $first_name = $request->get('first_name');
        $twitter = $request->get('twitter');
-       $password = $request->get('password');
        $git = $request->get('git');
        $google = $request->get('google');
        $avatar = $request->get('image');
@@ -132,97 +131,91 @@ class AdminController extends Controller
 
       $db_username = $query->getResult();
 
-      if (isset($_FILES['image'])) {
-        if (!empty($_FILES['image'])) {
-          if (is_valid_type($_FILES['image'])){
-            if (!file_exists($_FILES['image']['name'])){
-              $extension = strtolower(substr(strrchr($_FILES['image']['name'], '.'), 1));
-              $filename = DFileHelper::getRandomFileName($extension);
-              $target =  'img/avatar/' . $filename . '.' . $extension;
-              if (move_uploaded_file($_FILES['image']['tmp_name'],$target)){
-               if(isset($last_name) && isset($first_name) && isset($twitter) && isset($password) && isset($git) && isset($google)){
-                if(!empty($last_name) ){
-                  if(!empty($first_name)){
-                    if(!empty($twitter)){
-                      if(!empty($password)){
-                        if(!empty($git)){
-                          if(!empty($google)){
+        #if (isset($_FILES['image'])) {
+        #if (!empty($_FILES['image'])) {
+         # if (is_valid_type($_FILES['image'])){
+          #  if (!file_exists($_FILES['image']['name'])){
+             # $extension = strtolower(substr(strrchr($_FILES['image']['name'], '.'), 1));
+             # $filename = DFileHelper::getRandomFileName($extension);
+            # $target =  'img/avatar/' . $filename . '.' . $extension;
+             # if (move_uploaded_file($_FILES['image']['tmp_name'],$target)){
+      if(isset($last_name) && isset($first_name) && isset($twitter) && isset($git) && isset($google)){
+        if(!empty($last_name) ){
+          if(!empty($first_name)){
+            if(!empty($twitter)){
+              if(!empty($git)){
+                if(!empty($google)){
 
-                            $em = $this->getDoctrine()->getManager();
-                            $user = $em->getRepository('AppBundle:user')->find($user->id);
-                            $user->setUsername($user->username);
-                            $user->setlast_name($last_name);
-                            $user->settwitter($twitter);
-                            $user->setgoogle($google);
-                            $user->setgit($git);
-                            $user->setrole($user->role);
-                            $passwords = $this->get('security.password_encoder')
-                            ->encodePassword($user,$password);
-                            $user->setPassword($passwords);
-                            $user->setfirst_name($first_name);
-                            $user->setavatar('/img/avatar/'.$filename. '.' . $extension);
-                            $em->persist($user);
-                            $em->flush();
+                  $em = $this->getDoctrine()->getManager();
+                  $user = $em->getRepository('AppBundle:user')->find($user->id);
+                  $user->setUsername($user->username);
+                  $user->setlast_name($last_name);
+                  $user->settwitter($twitter);
+                  $user->setgoogle($google);
+                  $user->setgit($git);
+                  $user->setrole($user->role);
+                  $user->setfirst_name($first_name);
+                  $em->persist($user);
+                  $em->flush();
 
-                            $message['success'] = "User updated";
-                          }else{$message['danger'] = "google missing";}
-                        }else{$message['danger'] = "git missing";}
-                      }else{$message['danger'] = "password missing";}
-                    }else{$message['danger'] = "twitter missing";}
-                  }else{$message['danger'] = "first_name missing";}
-                }else{$message['danger'] = "last_name missing";}
-              }else{$message['danger'] = "Somsing missing";}
-            }else{$message['danger'] = "You can not download the file. Check permissions to the directory ( read / write)";}
-          }else{$message['danger'] = "File with this name already exists";}
-        }else{$message['danger'] = "You can upload files : JPEG, GIF, BMP, PNG";}
+                  $message['success'] = "User updated";
+                }else{$message['danger'] = "google missing";}
+              }else{$message['danger'] = "git missing";}
+            }else{$message['danger'] = "twitter missing";}
+          }else{$message['danger'] = "first_name missing";}
+        }else{$message['danger'] = "last_name missing";}
       }
+           # }else{$message['danger'] = "You can not download the file. Check permissions to the directory ( read / write)";}
+         # }else{$message['danger'] = "File with this name already exists";}
+        #}else{$message['danger'] = "You can upload files : JPEG, GIF, BMP, PNG";}
+      #}
+   # }
+
+      $repository = $this->getDoctrine()
+      ->getRepository('AppBundle:user');
+
+      $query = $repository->createQueryBuilder('p')
+      ->where('p.id = :id')
+      ->setMaxResults(1)
+      ->setParameter('id',$user )
+      ->getQuery();
+
+      $user_profile = $query->getResult();
+
+      $repository = $this->getDoctrine()
+      ->getRepository('AppBundle:photo');
+      $query = $repository->createQueryBuilder('p')
+      ->where('p.username = :id')
+      ->setParameter('id',$user )
+      ->getQuery();
+      $photo = $query->getResult();
+
+      $qb = $repository->createQueryBuilder('a');
+      $qb->select('COUNT(a)');
+      $qb->where('a.username = :usernameId');
+      $qb->setParameter('usernameId', $user );
+
+      $photos = $qb->getQuery()->getSingleScalarResult();
+
+      $paginator  = $this->get('knp_paginator');
+      $pagination = $paginator->paginate(
+        $photo,$request->query->getInt('page', 1),8);
+
+      if($user->role >= 2):
+        return $this->render('admin/admin.profile.html.twig', array(
+         'user'     => $user_profile,
+         'photo'    => $photo,
+         'photos'   => $photos,
+         'title'    => $user,
+         'message'  =>  $message,
+         'pagination' => $pagination,
+         'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+         ));
+      else:
+        return $this->redirectToRoute('home');
+      endif;
     }
-
-    $repository = $this->getDoctrine()
-    ->getRepository('AppBundle:user');
-
-    $query = $repository->createQueryBuilder('p')
-    ->where('p.id = :id')
-    ->setMaxResults(1)
-    ->setParameter('id',$user )
-    ->getQuery();
-
-    $user_profile = $query->getResult();
-
-    $repository = $this->getDoctrine()
-    ->getRepository('AppBundle:photo');
-    $query = $repository->createQueryBuilder('p')
-    ->where('p.username = :id')
-    ->setParameter('id',$user )
-    ->getQuery();
-    $photo = $query->getResult();
-
-    $qb = $repository->createQueryBuilder('a');
-    $qb->select('COUNT(a)');
-    $qb->where('a.username = :usernameId');
-    $qb->setParameter('usernameId', $user );
-
-    $photos = $qb->getQuery()->getSingleScalarResult();
-
-    $paginator  = $this->get('knp_paginator');
-    $pagination = $paginator->paginate(
-      $photo,$request->query->getInt('page', 1),8);
-
-    if($user->role >= 2):
-      return $this->render('admin/admin.profile.html.twig', array(
-       'user'     => $user_profile,
-       'photo'    => $photo,
-       'photos'   => $photos,
-       'title'    => $user,
-       'message'  =>  $message,
-       'pagination' => $pagination,
-       'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
-       ));
-    else:
-      return $this->redirectToRoute('home');
-    endif;
-  }
-  /*********************************************************************************************************/   
+    /*********************************************************************************************************/   
     /**
      *@Route("/admin/users",name="admin_users")
      */
@@ -250,7 +243,6 @@ class AdminController extends Controller
     /**
      *@Route("/admin/user/{id}",name="admin_user")
     */
-
     /*********************************************************************************************************/   
 
     public function  userAction(Request $request,$id)
@@ -705,5 +697,9 @@ class AdminController extends Controller
         return $this->redirectToRoute('home');
       endif;
     }
+
+
+
+
 
   }
